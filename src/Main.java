5bd0e633f3546c463c8c507bee793af13447d49a
even0dd <102162944+even0dd@users.noreply.github.com>
2022-07-02 21:34:31 +0600

    Kaiypbekov Bakay

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,15 @@ public class Main {
     static LinkedList<Person> p = new LinkedList<>();
     public static void main(String[] args) {
         personAdd(new Person("Alex","Dmitriev","Petrovich"),
-                new Person("Ale","Dmitriev","Petrovich"),
+                new Person("Alex","Dmitriev","Petrovich"),
                 new Person("Dmitrii","Seregeev","Kirillov"));
 
     }
     public static void personAdd(Person person,Person person1,Person person2) {
-
-        for (int i = 0; i<(p.size());i++) {
+        p.add(person2);
+        p.add(person1);
+        p.add(person);
+        for (int i = 0; i!=(p.size()-1);i++) {
             if (p.get(i).getName().equals(p.get(i+1).getName())&&
                     p.get(i).getSurname().equals(p.get(i+1).getSurname())&&
                     p.get(i).getPatronymic().equals(p.get(i+1).getPatronymic())) {
@@ -20,9 +22,7 @@ public class Main {
 
         }
         System.out.println("Everything is ok");
-        p.add(person2);
-        p.add(person1);
-        p.add(person);
+
 
     }
 }
