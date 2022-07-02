c493c8179f8330ab96ff8a60bbea04d19bb8b9df
even0dd <102162944+even0dd@users.noreply.github.com>
2022-07-02 21:32:13 +0600

    Kaiypbekov Bakay

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,28 @@
+
 import java.util.LinkedList;
 
 public class Main {
+    static LinkedList<Person> p = new LinkedList<>();
     public static void main(String[] args) {
-        LinkedList<Person> p = new LinkedList<>();
-        Person person = new Person("Bob", "Bob", "Bob");
-        Person person1 = new Person("Bo", "Bob", "Bob");
-        addPerson(person, p);
-        addPerson(person1, p);
+        personAdd(new Person("Alex","Dmitriev","Petrovich"),
+                new Person("Ale","Dmitriev","Petrovich"),
+                new Person("Dmitrii","Seregeev","Kirillov"));
+
     }
+    public static void personAdd(Person person,Person person1,Person person2) {
 
-    public static void addPerson(Person person, LinkedList<Person> p) {
-        for (int i = 0; i < p.size(); i++) {
-            if (p.get(i).getName().equals(person.getName())
-                    && p.get(i).getPatronymic().equals(person.getPatronymic()) &&
-                    p.get(i).getSurname().equals(person.getSurname())) {
-                throw new Error("Two people duplicated");
-            } else {
-                p.add(person);
-                System.out.println("Everything is ok");
-                break;
-
+        for (int i = 0; i<(p.size());i++) {
+            if (p.get(i).getName().equals(p.get(i+1).getName())&&
+                    p.get(i).getSurname().equals(p.get(i+1).getSurname())&&
+                    p.get(i).getPatronymic().equals(p.get(i+1).getPatronymic())) {
+                throw new Error("Two people is duplicated");
             }
+
         }
+        System.out.println("Everything is ok");
+        p.add(person2);
+        p.add(person1);
+        p.add(person);
 
     }
 }
